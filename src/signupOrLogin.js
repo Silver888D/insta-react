904454931ea6ba26ed1840773647f27a00abb3ff
Seb Amos <sebastian.amos8@gmail.com>
2022-08-15 16:22:09 +0100

    check

--- src/signupOrLogin.js
+++ src/signupOrLogin.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { signUp } from "./utils";
+import { login, signUp } from "./utils";
 
 const SignupOrLogin = ({setter})=>{
     const [username, setUsername] = useState();
@@ -9,8 +9,13 @@ const SignupOrLogin = ({setter})=>{
         event.preventDefault();
         await signUp(username, email, password, setter);
     }
+
+    const loginHandler= async(event)=>{
+        event.preventDefault();
+        await login(username, email, password, setter);
+    }
     return(
-        
+        <div>
         <form onSubmit={submitHandler}>
             <label>Create account details:</label><br></br>
             <input onChange={(event)=>setUsername(event.target.value)} placeholder="Username"></input><br></br>
@@ -19,6 +24,16 @@ const SignupOrLogin = ({setter})=>{
             <button type="submit">Submit</button>
             
         </form>
+
+<form onSubmit={loginHandler}>
+<label>LOGIN account details:</label><br></br>
+<input onChange={(event)=>setUsername(event.target.value)} placeholder="Username"></input><br></br>
+<input onChange={(event)=>setEmail(event.target.value)} type='email' placeholder="Email"></input><br></br>
+<input onChange={(event)=>setPassword(event.target.value)} placeholder="Password"></input><br></br>
+<button type="submit">Submit</button>
+
+</form>
+</div>
     );
 }
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -17,6 +17,27 @@ exports.signUp = async(userName, eMail, passWord, setter)=>{
     }
 };
 
+exports.login = async(userName, eMail, passWord, setter)=>{
+    try {
+        const res = await fetch('http://localhost:5000/login',{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'username':userName,
+                'email':eMail,
+                'password':passWord,
+                
+            })
+        });
+        const data = await res.json();
+        console.log(data.users);
+        setter(data.users.username);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
 exports.allUsers = async(setter)=>{
     try {
         const res = await fetch('http://localhost:5000/user',{
